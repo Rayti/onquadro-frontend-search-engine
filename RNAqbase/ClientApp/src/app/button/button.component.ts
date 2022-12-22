06350e2d573b0635929c600698294e17360f9d19
Filip <fil.kokosza@gmail.com>
2022-12-22 23:34:07 +0100

    ONQ-65 Added collecting of values from search page

--- RNAqbase/ClientApp/src/app/button/button.component.ts
+++ RNAqbase/ClientApp/src/app/button/button.component.ts
@@ -14,23 +14,10 @@ export class ButtonComponent {
   constructor(private http: HttpClient) { }
   clickEvent() {
     if (this.label === 'Search') {
-      this.getResults();
+      this.buttonClickedEvent.emit({ search: true, reset: false });
     }
     else if (this.label === 'Reset') {
       this.buttonClickedEvent.emit({ search: false, reset: true });
     }
-  }
-  getResults() {
-    this.http.post('http://localhost:5000/api/Search/PostAndGetResults',
-      `[
-        {"Attribute": "Loop Length", "Conditions": [{ "Value": "1", "Operator": ">=" }, { "Value": "6", "Operator": "<=" }]},
-        {"Attribute": "Experimental Method", "Conditions": [{ "Value": "X-Ray", "Operator": "" }]},
-        {"Attribute": "ONZ class", "Conditions": [{ "Value": "N-", "Operator": "" }, { "Value": "Z-", "Operator": "" }]},
-        {"Attribute": "PDB ID", "Conditions": [{ "Value": "10", "Operator": "" }]},
-        {"Attribute": "Number of tetrads", "Conditions": [{ "Value": "1", "Operator": ">" }]},
-        {"Attribute": "Type (by no. of strands)", "Conditions": [{ "Value": "tetramolecular", "Operator": "" }]},
-        {"Attribute": "Bulges", "Conditions": [{ "Value": "with bulges", "Operator": "" }]}
-        ]`)
-      .subscribe(data => console.log(JSON.stringify(data)));
   }
 }
